d8725139f62f9cda4ab5e777b9f4afaaf6e99d3f
Larskrs <lkssyv@gmail.com>
2023-07-01 00:34:40 +0200

    fuck

--- components/AktueltTVLoading.jsx
+++ components/AktueltTVLoading.jsx
@@ -10,7 +10,7 @@ export default function Layout() {
     return (
             <div className={styles.loading}>
                 <Image className={styles.bg} width={2000} height={2000} src={"/images/loadingbg.jpg"}></Image>
-                <Image className={styles.logo} width={200} height={200} src={"/aktueltstudios.svg"}></Image>
+                <Image className={styles.logo} width={200} height={200} src={"/aktueltstudio_logo.svg"}></Image>
             </div>
     );
 }
