6d9a0827b2fff82b94cf32e2d38cb2852aa9f430
Larskrs <lkssyv@gmail.com>
2023-08-24 22:34:17 +0200

    Added filters to series api

--- pages/api/v1/media/series/index.js
+++ pages/api/v1/media/series/index.js
@@ -5,15 +5,25 @@ import { GetClient } from "@/lib/Supabase";
 
 export default async function handler(req, res) {
 
+      const { limit=10, order="new" } = req.query
+      const orderParam = () => {
+        if (order == "new") {
+          return {table: "created_at", ascending: false}
+        }
+        if (order == "old") {
+          return {table: "created_at", ascending: true}
+        }
+      }
+ 
       let { data, error } = await GetClient("public")
       .from("series")
       .select(`
         *,
         episodes (*)
       `)
+      .limit(limit)
+      .order(orderParam().table, {ascending: orderParam().ascending})
       .eq("storage", process.env.NEXT_PUBLIC_STORAGE_ID)
-
-      console.log({data}, "Series API?")
       
       res.status(200).json({ data, error });
     
