8657804218af12d9d72c8d9aa7503d14528c7fe4
Larskrs <lkssyv@gmail.com>
2023-08-28 16:32:51 +0200

    Added type filters for series

--- pages/api/v1/media/series/index.js
+++ pages/api/v1/media/series/index.js
@@ -6,6 +6,8 @@ import { GetClient } from "@/lib/Supabase";
 export default async function handler(req, res) {
 
       const { limit=10, order="new" } = req.query
+      let allowedTypes = ["music","series","film", null, ]
+      if (req.query.allowedTypes != null) allowedTypes = req.query.allowedTypes.split(",")
       const orderParam = () => {
         if (order == "new") {
           return {table: "created_at", ascending: false}
@@ -24,7 +26,8 @@ export default async function handler(req, res) {
       .limit(limit)
       .order(orderParam().table, {ascending: orderParam().ascending})
       .eq("storage", process.env.NEXT_PUBLIC_STORAGE_ID)
+      .in("type", allowedTypes)
       
-      res.status(200).json({ data, error });
+      res.status(200).json({ data, error});
     
 }
