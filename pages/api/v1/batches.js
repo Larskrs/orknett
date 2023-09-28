f3c9ac0bf6b99f0cd6a9d2fbb55e76e4980b265e
Larskrs <lkssyv@gmail.com>
2023-09-28 23:22:39 +0200

    Alleine

--- pages/api/v1/batches.js
+++ pages/api/v1/batches.js
@@ -33,6 +33,10 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Credentials', true)
     res.setHeader('Access-Control-Allow-Origin', '*') // replace this your actual origin
     res.setHeader('Access-Control-Allow-Methods', 'GET')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
     res.status(200).json({batches: data})
   }
 
