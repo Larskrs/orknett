bbaf146468601e4b58e7fbc895c55f05b1d78a25
Larskrs <lkssyv@gmail.com>
2023-10-30 22:33:36 +0100

    Sylt in the bag

--- pages/video.js
+++ pages/video.js
@@ -1,7 +1,7 @@
 
 import { VideoPlayer } from "@/components";
 
-export default function() {
+export default function VideoPage() {
     return (
         <div style={{maxWidth: "90vw", maxHeight: "90vh"}}>
             <VideoPlayer videoProps={{loop: true}} source={"http://aktuelt.tv/api/v1/files/videos?quality=360&fileId=ffc422c6-b6a4-4e09-810d-9cd361d1d0c0.mov"} />
