bca9e2884a595191af35d557fadad1a7f1def86b
Larskrs <lkssyv@gmail.com>
2023-11-03 18:35:52 +0100

    Revamped the look of the batches page

--- pages/dashboard/batches/index.js
+++ pages/dashboard/batches/index.js
@@ -10,6 +10,7 @@ import { contentTypeList, isSourceContentType } from "@/lib/ExtensionHelper";
 import { GetShortHandle } from "@/lib/ShorthandHelper";
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
+import { useState, useEffect } from "react";
 
 
 function BatchList ({batches}) {
@@ -19,10 +20,10 @@ function BatchList ({batches}) {
         return (
 
             <Link key={i} href={"/dashboard/batches/" + batch.id}>
-                <div  className={styles.article}>
-                    <Image className={styles.thumbnail} src={batch.thumbnail} alt={batch.thumbnail} fill />
+                <div className={styles.article}>
+                    <Image style={{objectFit: "contain"}}  src={batch.thumbnail} alt={batch.thumbnail} fill />
                 </div>
-                <p style={{zIndex: 5, color: "#ccc", position: "relative"}}>{batch.title} </p>
+                <p style={{zIndex: 5, color: "#777", position: "relative"}}>{batch.title} </p>
             </Link>
 
             // <Badge style={{cursor: "pointer", overflow: "hidden"}} key={batch.id} onClick={() => router.push("/dashboard/batches/" + batch.id)}>
@@ -40,27 +41,64 @@ function BatchesPage ({batches}) {
     const session = useSession()
 
     let selfBatches = []
-    let publicBatches = batches; 
+    let sharedBatches = batches; 
     if (session.status === "authenticated") {
-        selfBatches = batches.filter((b) => b?.owners?.includes(session.data.user.id))
-        publicBatches = batches.filter((b) => !b?.owners?.includes(session.data.user.id) && b.data?.access !== "private")
+        selfBatches = batches.filter((b) => b?.owner === session.data.user.id)
+        sharedBatches = batches.filter((b) => b?.owners.includes(session.data.user.id) && b?.owner !== session.data.user.id)
     }
+
+    const [batchPreview, setBatchPreview] = useState(0)
+
+    useEffect(() => {
+    
+        // console.log(videoRef.current.duration);
+        const interval = setInterval(() => {
+            console.log("SEX")
+            let id = batchPreview + 1
+            if (id > batches.length -1) {
+                id = 0
+            }
+            setBatchPreview(id)
+        },3000);
+        return () => clearInterval(interval);
+      }, [batchPreview]);
+
+      
 
     return (
         <FileSharingLayout pageId={3} batches={batches}>
             <div className="grid">
-                <div className="side">
+                {/* <div className="side">
                    {session.status === "authenticated" && <BatchList batches={selfBatches} />}
                    {session.status !== "authenticated" && <p>You need to be logged in to create your own batches.</p>}
-                </div>
-                <div className={`${styles.wrap} main`}> 
+                </div> */}
+                <div className={`main`}> 
+
+                    <div className="header">
+                        <div className="background">
+                            {batches.map((b, i) => {
+                               return  <Image id={b.id} alt={"Batch header"} fill style={{objectFit: "cover", translate: batchPreview != i ? "100% 0": "0%", opacity: batchPreview == i ? 1: 0}} src={b.thumbnail}/>
+                            })
+
+                            }
+                        </div>
+                        <div className="info">
+                            <h1>{batches?.[batchPreview].title}</h1>
+                            <p>{batches?.[batchPreview].owners.length} participent{batches?.[batchPreview].owners.length == 1 ? "" : "s"}</p>
+                        </div>
+                    </div>
 
                     <div className="head">
-                        <h2>Offentlige</h2>
-                        <hr />
+                        <h3>Dine ({selfBatches.length})</h3>
                     </div>
                     <div className="public">
-                        <BatchList batches={publicBatches} />
+                        <BatchList batches={selfBatches} />
+                    </div>
+                    <div className="head">
+                        <h3>Delt med deg ({sharedBatches.length})</h3>
+                    </div>
+                    <div className="public">
+                        <BatchList batches={sharedBatches} />
                     </div>
                 </div>
 
@@ -75,33 +113,69 @@ function BatchesPage ({batches}) {
                     flex-wrap: wrap;
                     gap: 2rem;
                     padding: 0 !important;
-                    border: none !important;
+                    border: none;
+                    border-radius: 0 !important;
+                    border-bottom: 1px solid #999;
+                    padding: 1rem;
                 }
                 .grid {
-                    display: grid;
+                    display: flex;
                     width: 100%;
                     height: 100%;
                     min-height: 100vh;
-                    grid-template-columns: 500px 1fr;
-                    grid-template-rows: 200px 1fr;
-                    grid-template-areas: 
-                        "side main"
-                        "side main";
                     gap: 1rem;
                     padding: 1rem;
                     {/* position: fixed; */}
                 }
                 .grid div {
                     background: rgba(255, 255, 255, 0);
-                    border-radius: 16px;
+                    border-radius: 8px;
                     box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
                     backdrop-filter: blur(5px);
                     -webkit-backdrop-filter: blur(5px);
-                    border: 1px solid rgba(255, 255, 255, 0.3);
-                    padding: 1rem;
+                    {/* border: 1px solid rgba(255, 255, 255, 0.3); */}
+                    {/* padding: 1rem; */}
                 }
-                .side {
-                    grid-area: side;
+                .head {
+                        flex-direction: column !important;
+                        width: 100%;
+                        border-radius: 0px !important;
+                }
+                .header {
+                    height: 250px;
+                    width: 100%;
+                    padding: 1rem;
+                    background: rgba(255, 255, 255, .5)
+                }
+                .header * {
+                    z-index: 2;
+                    position: relative;
+                }
+                .header .background {
+                    top: 0;
+                    left: 0;
+                    z-index: 0;
+                    position: absolute;
+                    width: 100%;
+                    height: 100%;
+                    border-radius: 8px;
+                    overflow: hidden;
+                    opacity: 0.5;
+                }
+                .header .info {
+                    position: absolute;
+                    left: 50%;
+                    top: 50%;
+                    margin: 0;
+                    translate: -50% -50%;
+                    background: transparent;
+                    padding: 1rem 2rem;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    flex-direction: column;
+                    text-align: center;
+                    box-shadow: none;
                 }
                 .main {
                     display: flex;
@@ -111,43 +185,12 @@ function BatchesPage ({batches}) {
                     align-items: flex-start;
                     overflow: scroll;
                     flex-direction: column;
+                    gap: 1rem;
                 }
-                .head {
-                    background: rgba(255,255,255,0.1) !important;
-                    border: none !important;
-                    flex-direction: column !important;
-                    width: 100%;
-                }
-
-                @media screen and (max-height: 600px) {
-                    
-                }
-                @media screen and (max-width: 1100px) {
-                    .grid {
-                        grid-template-columns: 300px 1fr;
-                        grid-template-rows: 1fr 1fr;
-                        grid-template-areas: 
-                            "side main"
-                            "side main";
-                    } 
-                }
-                @media screen and (max-width: 700px) {
-                    .grid {
-                        grid-template-columns: 1fr 1fr;
-                        grid-template-rows: auto 1fr;
-                        grid-template-areas: 
-                            "side side"
-                            "main main";
-                    } 
-                    .main, .side div {
-                        display: flex;
-                        flex-direction: column;
-                        align-items: center;
-                        width: 100%;
-                    }
-                    .main div {
-                        width: 100%;
-                    }
+                h3 {
+                    font-size: 18px;
+                    margin: 0;
+                    font-weight: 400;
                 }
             `}</style>
         </FileSharingLayout>
