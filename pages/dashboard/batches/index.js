cff0df0246aa68ce35eec5656f5d023661fc30db
Larskrs <lkssyv@gmail.com>
2023-11-05 16:40:42 +0100

    Improved the batches page for mobile and pc

--- pages/dashboard/batches/index.js
+++ pages/dashboard/batches/index.js
@@ -1,7 +1,7 @@
 import { GetAuthenticatedClient, GetClient } from "@/lib/Supabase";
 import FileSharingLayout from "@/layouts/FileSharingLayout";
 import Link from "next/link";
-import styles from '@/styles/FileSharing.module.css'
+import styles from '@/styles/Batches.module.css'
 import { GetCookie } from "@/lib/CookieHelper";
 import Image from "next/image";
 import { Badge, ColorImage } from "@/components";
@@ -19,11 +19,14 @@ function BatchList ({batches}) {
         
         return (
 
-            <Link key={i} href={"/dashboard/batches/" + batch.id}>
-                <div className={styles.batchElement}>
+            <Link className={styles.article} key={i} href={"/dashboard/batches/" + batch.id}>
+                <div className={styles.thumbnail}>
                     <Image src={batch.thumbnail} alt={batch.thumbnail} fill />
                 </div>
-                <p style={{zIndex: 5, color: "#777", position: "relative"}}>{batch.title} </p>
+                <div className={styles.info}>
+                    <p className={styles.title}>{batch.title} </p>
+                    <p>{batch.owners.length} member{batch.owners.length > 1 ? "s" : ""}</p>
+                </div>
             </Link>
 
             // <Badge style={{cursor: "pointer", overflow: "hidden"}} key={batch.id} onClick={() => router.push("/dashboard/batches/" + batch.id)}>
@@ -35,7 +38,7 @@ function BatchList ({batches}) {
     </>
 }
 
-function BatchesPage ({batches}) {
+function BatchesPage ({batches, thumbnailBatches}) {
 
     const router = useRouter()
     const session = useSession()
@@ -55,7 +58,7 @@ function BatchesPage ({batches}) {
         const interval = setInterval(() => {
             console.log("SEX")
             let id = batchPreview + 1
-            if (id > batches.length -1) {
+            if (id > thumbnailBatches.length -1) {
                 id = 0
             }
             setBatchPreview(id)
@@ -63,41 +66,48 @@ function BatchesPage ({batches}) {
         return () => clearInterval(interval);
       }, [batchPreview]);
 
-      
-
     return (
         <FileSharingLayout pageId={3} batches={batches}>
-            <div className="grid">
+            <div className={styles.grid}>
                 {/* <div className="side">
                    {session.status === "authenticated" && <BatchList batches={selfBatches} />}
                    {session.status !== "authenticated" && <p>You need to be logged in to create your own batches.</p>}
                 </div> */}
-                <div className={`main`}> 
+                <div className={styles.main}> 
 
-                    <div className="header">
-                        <div className="background">
-                            {batches.map((b, i) => {
+                    <div className={styles.header}>
+                        
+                        <div className={styles.background}>
+                            {thumbnailBatches.map((b, i) => {
                                return  <Image key={b.id} alt={"Batch header"} fill style={{objectFit: "cover", borderRadius: 8, transitionDuration: "1s", translate: batchPreview != i ? "100% 0": "0%", opacity: batchPreview == i ? 1: 0}} src={b.thumbnail}/>
                             })
 
                             }
                         </div>
-                        <div className="info">
-                            <h1>{batches?.[batchPreview].title}</h1>
-                            <p>{batches?.[batchPreview].owners.length} participent{batches?.[batchPreview].owners.length == 1 ? "" : "s"}</p>
+                        
+                        <div className={styles.info}>
+                            <div className={styles.previewItem}>
+                                {thumbnailBatches.map((b, i) => {
+                                   return  <Image key={b.id} alt={"Batch header"} fill style={{objectFit: "cover", borderRadius: 8, transitionDuration: "1s", translate: batchPreview != i ? "-100% 0": "0%", scale: batchPreview != i ? "0": "1", opacity: batchPreview == i ? 1: 0}} src={b.thumbnail}/>
+                                })}
+                            </div>
+                            <div className={styles.details}>
+                                <h1>{thumbnailBatches?.[batchPreview].title}</h1>
+                                <p>{thumbnailBatches?.[batchPreview].owners.length} member{thumbnailBatches?.[batchPreview].owners.length == 1 ? "" : "s"}</p>
+                            </div>
                         </div>
                     </div>
 
-                    <div className="head">
+                    <div className={styles.head}>
                         <h3>Dine ({selfBatches.length})</h3>
                     </div>
-                    <div className="public">
+                    <div className={styles.public}>
                         <BatchList batches={selfBatches} />
                     </div>
-                    <div className="head">
+                    <div className={styles.head}>
                         <h3>Delt med deg ({sharedBatches.length})</h3>
                     </div>
-                    <div className="public">
+                    <div className={styles.public}>
                         <BatchList batches={sharedBatches} />
                     </div>
                 </div>
@@ -106,94 +116,7 @@ function BatchesPage ({batches}) {
             </div>
 
             <style jsx>{`
-                .public {
-                    width: 100%;
-                    display: flex;
-                    flex-direction: row;
-                    flex-wrap: wrap;
-                    gap: 2rem;
-                    padding: 0 !important;
-                    border: none;
-                    border-radius: 0 !important;
-                    border-bottom: 1px solid #999;
-                    padding: 1rem;
-                }
-                .grid {
-                    display: flex;
-                    width: 100%;
-                    height: 100%;
-                    min-height: 100vh;
-                    gap: 1rem;
-                    padding: 1rem;
-                    {/* position: fixed; */}
-                }
-                .grid div {
-                    background: rgba(255, 255, 255, 0);
-                    border-radius: 8px;
-                    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-                    position: relative;
-                    backdrop-filter: blur(5px);
-                    -webkit-backdrop-filter: blur(5px);
-                    {/* border: 1px solid rgba(255, 255, 255, 0.3); */}
-                    {/* padding: 1rem; */}
-                }
-                .head {
-                        flex-direction: column !important;
-                        width: 100%;
-                        border-radius: 0px !important;
-                }
-                .header {
-                    height: 250px !important;
-                    max-height: 250px !important;
-                    width: 100%;
-                    padding: 1rem;
-                    background: rgba(255, 255, 255, .5)
-                }
-                .header * {
-                    z-index: 2;
-                    position: relative;
-                }
-                .header .background {
-                    top: 0;
-                    left: 0;
-                    z-index: 0;
-                    position: absolute;
-                    width: 100%;
-                    height: 100%;
-                    border-radius: 8px;
-                    overflow: hidden;
-                    opacity: 0.5;
-                }
-                .header .info {
-                    position: absolute;
-                    left: 50%;
-                    top: 50%;
-                    margin: 0;
-                    translate: -50% -50%;
-                    background: transparent;
-                    padding: 1rem 2rem;
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                    flex-direction: column;
-                    text-align: center;
-                    box-shadow: none;
-                }
-                .main {
-                    display: flex;
-                    width: 100%;
-                    grid-area: main;
-                    justify-content: flex-start;
-                    align-items: flex-start;
-                    overflow: scroll;
-                    flex-direction: column;
-                    gap: 1rem;
-                }
-                h3 {
-                    font-size: 18px;
-                    margin: 0;
-                    font-weight: 400;
-                }
+                
             `}</style>
         </FileSharingLayout>
     );
@@ -223,18 +146,21 @@ export async function getServerSideProps(ctx){
             if (isSourceContentType(file.source, "image")) { thumbnail = file.source ; break; }
             if (isSourceContentType(file.source, "audio")) { thumbnail = `/api/v1/files/audio/cover?fileId=${file.source.split("fileId=").pop()}` ; break; }
             if (isSourceContentType(file.source, "video")) { thumbnail = `/api/v1/files/videos/thumbnail?fileId=${file.source.split("fileId=").pop()}` ; break; }
-            thumbnail = ""
+            
             break
-
         }
 
         if (thumbnail !== "")
         data[i].thumbnail = thumbnail
     }
 
+    const thumbnailBatches = data.filter((b) => b.thumbnail !== undefined)
+    console.log(thumbnailBatches.map((b) => { return b.thumbnail}))
+
     return {
         props:{
             batches: data,
+            thumbnailBatches
         }
     }
 
