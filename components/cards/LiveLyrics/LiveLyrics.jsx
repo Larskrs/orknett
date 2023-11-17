2408d9d82a42ec63b2f4bee4df7e45e0f32714bc
Larskrs <lkssyv@gmail.com>
2023-11-17 18:08:14 +0100

    Soeg

--- components/cards/LiveLyrics/LiveLyrics.jsx
+++ components/cards/LiveLyrics/LiveLyrics.jsx
@@ -1,31 +1,75 @@
 
+import { Londrina_Sketch } from "next/font/google";
 import { useState, useEffect } from "react";
 import styles from "./LiveLyrics.module.css"
 
-export default function LiveLyrics ({lyrics, currentMillis}) {
+export default function LiveLyrics ({lyrics, audioRef}) {
 
     const [currentLine, setCurrentLine] = useState(0)
+    const [currentVerse, setCurrentVerse] = useState(0)
+    const [currentMillis, setCurrentMillis] = useState(0)
+
+    const lines = [].concat(...lyrics);
+    console.log(lines)
 
     useEffect(() => {
-        const currentLineEl = lyrics[currentLine]
-        if (lyrics[currentLine])
-        for (let i = 0; i < lyrics.length; i++) {
-            const line = lyrics[i];
+        const interval = setInterval(() => {
+            setCurrentMillis(audioRef.current.currentTime)
+          },10);
+          return () => clearInterval(interval);
+    }, [currentMillis, audioRef]);
+
+    useEffect(() => {
+        audioRef.current.addEventListener('play', () => {
+            console.log("Starting playback...")
+            setCurrentLine(0)
+          });
+      }, [audioRef]);
+
+    useEffect(() => {
+        const currentLineEl = lines[currentLine]
+        if (lines[currentLine])
+        for (let i = 0; i < lines.length; i++) {
+            const line = lines[i];
             if (line.millis < currentMillis) {
-                setCurrentLine(i+1)
+                setCurrentLine(i)
+            } else {
+                continue;
             }
         }
 
     }, [currentLine, currentMillis])
 
-    return (
-        <div className={styles.wrap}>
-                    <p>Current Millis: [{currentMillis}]</p>
-                    <p>Current Line: [{currentLine}]</p>
+    function calculateOpacity(x, midpoint, maxOpacity) {
+        // Calculate the absolute difference between x and the midpoint
+        var difference = Math.abs(x - midpoint);
+    
+        // Calculate opacity based on the difference and a maximum opacity value
+        var opacity = 1 - (difference / (midpoint * 2)) * maxOpacity;
+    
+        // Ensure opacity is within the range [0, 1]
+        opacity = Math.max(0, Math.min(1, opacity));
+    
+        return opacity;
+    }
 
-                    {lyrics.map((line, i) => {
-                        return <p style={{opacity: currentLine == i ? 1 : .5}}> {line.line}</p>
-                    })}
+    return (
+        <div className={styles.verse}>
+
+                    {lines.map((line, i) => {
+                                            
+                        return <p style={{
+                            opacity: currentLine == i ? 1 : .5,
+                            display: "flex",
+                            gap: 8,
+                            transform: currentLine == i ? "scale(1.2)" : "scale(1)"
+                        }}
+                        onClick={() => {
+                            audioRef.current.pause()
+                            audioRef.current.currentTime = line.millis;
+                            audioRef.current.play()
+                        }}> {line.line}</p>
+                        })}
         </div>
     )
 
--- pages/dashboard/lyrics.jsx
+++ pages/dashboard/lyrics.jsx
@@ -4,40 +4,65 @@ import { useEffect, useRef, useState } from "react";
 
 export default function LyricsTest () {
 
-    const [currentMillis, setCurrentMillis] = useState(0)
     const audioRef = useRef(null)
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrentMillis(audioRef.current.currentTime)
-          },10);
-          return () => clearInterval(interval);
-    }, [currentMillis, audioRef]);
 
     return (<div>
         <h2>Live Lyric Test 1</h2>
-        <audio ref={audioRef} src="http://aktuelt.tv/api/v1/files?fileId=69a70312-01c5-407e-bd02-b3d7a70ab7b5.mp3" controls />
-        <LiveLyrics currentMillis={currentMillis} lyrics={[
-            {
-                millis: 14.5,
-                line: "(Intro)"
-            },
-            {
-                millis: 17,
-                line: "We in the chruch, playa, get your mail"
-            },
-            {
-                millis: 20,
-                line: "It's only two places you end up, either gay or in jail"
-            },
-            {
-                millis: 21.5,
-                line: "Still nowhere to go (nowhere to go)"
-            },
-            {
-                millis: 23.5,
-                line: "Still nowhere to go (nowhere to go)"
-            },
+        <audio ref={audioRef} style={{width: "100%"}}src="http://aktuelt.tv/api/v1/files?fileId=69a70312-01c5-407e-bd02-b3d7a70ab7b5.mp3" controls />
+        <LiveLyrics audioRef={audioRef} lyrics={[
+            [
+                {
+                    millis: 0,
+                    line: "(Intro)"
+                },
+                {
+                    millis: 14.4,
+                    line: "We in the chruch, playa, get your mail"
+                },
+                {
+                    millis: 16.75,
+                    line: "It's only two places you end up, either gay or in jail"
+                },
+                {
+                    millis: 19.5,
+                    line: "Still nowhere to go (nowhere to go)"
+                },
+                {
+                    millis: 22.25,
+                    line: "Still nowhere to go (nowhere to go)"
+                },
+                {
+                    millis: 25.25,
+                    line: "So keep your hands up"
+                },
+                {
+                    millis: 26,
+                    line: "Mormons, Muslims, Christians, hoes"
+                },
+                {
+                    millis: 29,
+                    line: "Everybody, fuck that"
+                },
+                {
+                    millis: 30.75,
+                    line: "Still nowhere to go (nowhere to go)"
+                },
+                {
+                    millis: 33.5,
+                    line: "Still nowhere to go (nowhere to go)"
+                },
+            ],
+            [
+                {
+                    millis: 36.5,
+                    line: "Two words, young Willy, spit fire, praise god"
+                },
+                {
+                    millis: 39.5,
+                    line: "get higher, admire my talents, stay balanced, get back me"
+                },
+            ],
+
         ]} />
     </div>)
 
