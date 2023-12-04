707f413c1751e63343de5a7ef7eb32142f658ef7
Larskrs <lkssyv@gmail.com>
2023-12-04 19:21:15 +0100

    Kill me

--- pages/dashboard/admin/index.jsx
+++ pages/dashboard/admin/index.jsx
@@ -1,16 +1,34 @@
 import { RadialProgress } from "@/components";
 import { getReadableFileSizeString } from "@/lib/FileHelper";
 import axios from "axios";
+import { useSession } from 'next-auth/react';
+import useFetch from "@/hooks/UseFetch";
+import { useEffect } from "react";
+import { Slider } from "@/components";
+import Image from "next/image";
 
 export default function AdminPage ({storage}) {
 
     const videosSize = getReadableFileSizeString(storage.videos)
     const filesSize = getReadableFileSizeString(storage.files)
 
-    return (
-        <div>
+    const session = useSession({
+        required: true
+    })
 
-        <section style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+    if (session.status === 'loading') {
+        return (
+            <section style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+                <p>Loading Page</p>
+            </section>
+        )
+    }
+
+
+    return (
+        <div style={{minHeight: "100vh"}}>
+            
+        <section style={{display: "flex", alignItems: "center", justifyContent: "center", height: "100vh"}}>
             <div style={{display: "flex", flexDirection: "column", gap: 8, alignItems: "center"}}>
                 <RadialProgress progress={(storage.videos / storage.allocated) * 100} trackWidth={9.5} indicatorWidth={10} label={"Videos"} trackColor={"#222"} indicatorColor={"var(--ak-primary)"}/>
                 <p>{videosSize}</p>
@@ -19,35 +37,34 @@ export default function AdminPage ({storage}) {
                 <RadialProgress progress={(storage.files / storage.allocated) * 100} trackWidth={9.5} indicatorWidth={10} label={"Files"} trackColor={"#222"} indicatorColor={"var(--folly)"}/>
                 <p>{filesSize}</p>
             </div>
-            <div style={{display: "flex", flexDirection: "column", gap: 8, alignItems: "center"}}>
-                <RadialProgress progress={storage.usage} trackWidth={9.5} indicatorWidth={10} label={"Files"} trackColor={"#222"} indicatorColor={"var(--folly)"}/>
-                <p>{filesSize}</p>
-            </div>
+            {session.status == "authenticated" && <FilesUploadAmount session={session} /> }
             
         </section>
 
-        <section style={{display: "flex", alignItems: "center", justifyContent: "center", flexWrap: "wrap"}}>
-            {storage.cpus.map((cpu, i) => { 
-                
 
-                const usage = (cpu.times.user/cpu.times.idle) * 100
-
-                return (
-                <div key={i} style={{display: "flex", flexDirection: "column", gap: 8, alignItems: "center"}}>
-                    <RadialProgress progress={usage}
-                        trackWidth={9.5} indicatorWidth={10} label={"Thread " + ( i + 1)} trackColor={"#222"}
-                        indicatorColor={"var(--folly)"}
-                        />
-                </div>
-                        
-                )
-            })
-            }
-            
-        </section>
+        
         </div>
     );
 }
+function FilesUploadAmount ({session}) {
+
+    const {  data, isLoading, error, refetch } = useFetch(`files/storage?userId=${session.data.user.id}`)
+    useEffect(() => {
+        console.log(data)
+    }, [data])
+
+    const fileSize = getReadableFileSizeString(data.files)
+
+    
+    return (
+
+            <div style={{display: "flex", flexDirection: "column", gap: 8, alignItems: "center"}}>
+                <RadialProgress progress={(data.files / 20000000000) * 100} trackWidth={9.5} indicatorWidth={10} label={"Your Files"} trackColor={"#222"} indicatorColor={"var(--ak-tertiary)"}/>
+                <p>{fileSize}</p>
+            </div>
+    )
+}
+
 
 const os = require("os")
 
