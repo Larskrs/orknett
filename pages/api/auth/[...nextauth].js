a9dafeb62c5bd251451bab62c5eb903e4092e0d9
Larskrs <lkssyv@gmail.com>
2023-12-15 19:14:32 +0100

    Slightly Improved Authentication

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -16,13 +16,6 @@ export const authOptions = {
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            authorization: {
-                params: {
-                    prompt: "consent",
-                    access_type: "offline",
-                    response_type: "code",
-                }
-            }
         })
     ],
     pages: {
