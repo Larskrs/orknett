25f8902e128ccf522a412eca34a0776def4e280f
Larskrs <lkssyv@gmail.com>
2024-01-29 13:17:16 +0100

    Improved click thang

--- components/cards/ClipBoardWrap/ClipboardWrap.jsx
+++ components/cards/ClipBoardWrap/ClipboardWrap.jsx
@@ -6,15 +6,20 @@ export default function ClipboardWrap ({data, children}) {
     const [copied, setCopied] = useState(false)
 
     useEffect(() => {
-        setTimeout(() => {
+
+        const timeout = setInterval(() => {
           setCopied(false);
         }, 5000);
+
+        return () => clearInterval(timeout)
      }, [copied]);
 
     return (
-        <div  onClick={() => {navigator.clipboard.writeText(data); setCopied(true)}}>
+        <div>
             <div className={styles.container}>
-            {children}
+            <div onClick={() => {navigator.clipboard.writeText(data); setCopied(true)}}>
+                {children}
+                </div>
             {copied && <div className={styles.pop_up}>
                 Kopiert til utklipstavle!
             </div>}
--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,13 @@
 import { ClipboardWrap, InputField } from "@/components";
+import Layout from "@/layouts/newUI/ArticleLayout";
 import styles from "@/styles/newUI/contact.module.css"
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Contact () {
     return (
+        <Layout>
+
         <div className={styles.container}>
             <div className={styles.grid}>
                 
@@ -13,15 +16,20 @@ export default function Contact () {
                 <div className={styles.information}>
                     <p>Kontakt oss på email</p>
                     <ClipboardWrap data={"aktueltstudio@gmail.com"}><p className={styles.detail}>aktueltstudio@gmail.com</p></ClipboardWrap>
+
+                    <p>What the fuck?</p>
                     </div>
 
                     <Link className={styles.action} href={"/"}>Hjem</Link>
                     </div>
                     <div className={styles.image}>
                         <Image className={styles.blur} fill src={"http://aktuelt.tv/api/v1/files?fileId=d8e5e400-ec12-4c7d-babe-3d95ad75d0e7.JPG"} />
-                        <Image fill src={"http://aktuelt.tv/api/v1/files?fileId=d8e5e400-ec12-4c7d-babe-3d95ad75d0e7.JPG"} />
+                        <Image fill src={"http://aktuelt.tv/api/v1/files?fileId=d8e5e400-ec12-4c7d-babe-3d95ad75d0e7.JPG"}/>
                     </div>
+
+
                 </div>
         </div>
+        </Layout>
     );
 }
